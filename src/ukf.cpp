a8b31e4b634997029610d07bdeebd4e9d3a5ced4
Vianney Monestel Alfaro <vmonestel@hpe.com>
2018-06-02 23:05:26 -0600

    Tune process parameters std_a_ and std_yawdd_
    * These parameters represent the process noise. Setting both
    to 0.5 reduces the RMSE value.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -30,10 +30,10 @@ UKF::UKF() {
            0,    0, 0, 0, 1;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 2;
+  std_a_ = 0.5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 1;
+  std_yawdd_ = 0.5;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
